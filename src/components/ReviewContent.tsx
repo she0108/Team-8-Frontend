b7825802d9576043a42ec9d234bae8146eca9918
manu1307 <jasonsc@naver.com>
2023-11-11 01:11:57 +0900

    Feat : 강의 DetailPage  UI 구현

---
+++ src/components/ReviewContent.tsx
@@ -0,0 +1,60 @@
+import React from "react";
+import Rating from "@mui/material/Rating";
+import { Icon, Text, Typography, VStack } from "@channel.io/bezier-react";
+import {
+  HandThumbdownFilledIcon,
+  HandThumbupFilledIcon,
+  HandThumbupIcon,
+} from "@channel.io/bezier-icons";
+
+type ReviewContentProps = {
+  id: string;
+  rating: number;
+  good_review: string;
+  bad_review: string;
+};
+const testData = {
+  id: "asdf",
+  rating: 4.5,
+  good_review:
+    "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae quia dolorem recusandae impedit perspiciatis quibusdam. Enim, facilis. Eos, unde error voluptatem a, ",
+  bad_review:
+    "Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore, nesciunt voluptatem non nisi placeat ducimus voluptas excepturi ",
+};
+function ReviewContent() {
+  // {
+  //   id,
+  //   rating,
+  //   good_review,
+  //   bad_review,
+  // }: ReviewContentProps
+  const { id, rating, good_review, bad_review } = testData;
+
+  return (
+    <div>
+      <div style={{ display: "flex", gap: "10px", alignItems: "center" }}>
+        <Text typo={Typography.Size16} bold={true}>
+          {id}
+        </Text>
+        <Rating value={2} readOnly />
+      </div>
+      <div
+        style={{
+          display: "flex",
+          gap: "15px",
+          marginTop: "10px",
+          marginBottom: "10px",
+        }}
+      >
+        <Icon source={HandThumbupFilledIcon} />
+        <Text>{good_review}</Text>
+      </div>
+      <div style={{ display: "flex", gap: "15px" }}>
+        <Icon source={HandThumbdownFilledIcon} />
+        <Text>{bad_review}</Text>
+      </div>
+    </div>
+  );
+}
+
+export default ReviewContent;
