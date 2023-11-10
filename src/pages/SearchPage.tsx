12d6b55b40dcd1f1f8a81c090609febfbdc985c5
she1018 <she020108@gmail.com>
2023-11-11 01:36:03 +0900

    fix: SearchPage 검색 아이콘 추가

--- src/pages/SearchPage.tsx
+++ src/pages/SearchPage.tsx
@@ -2,8 +2,11 @@ import FilterAreaButton from "@/components/FilterAreaButton";
 import FilterPriceButton from "@/components/FilterPriceButton";
 import FilterStackButton from "@/components/FilterStackButton";
 import LectureItem from "@/components/LectureItem";
+import { SearchIcon } from "@channel.io/bezier-icons";
 import {
   Divider,
+  Icon,
+  IconSize,
   Tag,
   TextField,
   TextFieldSize,
@@ -21,12 +24,22 @@ const SearchPage: React.FC = () => {
         gap: 12,
       }}
     >
-      <TextField
-        type={TextFieldType.Search}
-        size={TextFieldSize.L}
-        placeholder="강의 제목, 분야, 기술 스택을 검색하세요"
-        style={{ flexShrink: 0 }}
-      />
+      <div
+        style={{ display: "flex", flexDirection: "row", alignItems: "center" }}
+      >
+        <Icon
+          source={SearchIcon}
+          color="bgtxt-blue-normal"
+          size={IconSize.Normal}
+          style={{ marginRight: 8 }}
+        />
+        <TextField
+          type={TextFieldType.Search}
+          size={TextFieldSize.L}
+          placeholder="강의 제목, 분야, 기술 스택 등"
+          style={{ flexShrink: 0 }}
+        />
+      </div>
       <div
         style={{
           display: "flex",
